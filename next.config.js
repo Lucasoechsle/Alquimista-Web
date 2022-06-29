1cec1129984d373414aa648eb3054600fdbcf3b6
fpiantoni <fran@piantoni.com.ar>
2022-06-29 12:50:02 -0300

    Added base for PDF

--- next.config.js
+++ next.config.js
@@ -7,6 +7,26 @@ module.exports = {
         destination: '/en',
         permanent: true,
       },
+      {
+        source: '/manucoll',
+        destination: '/files/Bircle-Deck-Proto.pdf',
+        permanent: true,
+      },
+      {
+        source: '/alquimistanft',
+        destination: '/files/Bircle-Deck-Proto.pdf',
+        permanent: true,
+      },
+      {
+        source: '/alquimista',
+        destination: '/files/Bircle-Deck-Proto.pdf',
+        permanent: true,
+      },
+      {
+        source: '/bircle',
+        destination: '/files/Bircle-Deck-Proto.pdf',
+        permanent: true,
+      },
     ];
   },
 };
